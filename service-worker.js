5aead3d020ff719aa7b7a55271f787ab99147f91
Andy Fang <andyfang486@berkeley.edu>
2020-06-30 20:39:21 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/afang223/helloWorld/precache-manifest.24d1401d0c05ee06cfeb1737a46d5dc5.js"
+  "/afang223.github.com/helloWorld/precache-manifest.6eb9040f8f32c5ddbe4cab98134015a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/afang223/helloWorld/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/afang223.github.com/helloWorld/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

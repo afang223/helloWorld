cae8802d95dfc1b861b81161ab4ecce6fb478555
Andy Fang <andyfang486@berkeley.edu>
2020-06-30 20:11:24 -0700

    Updates

---
+++ precache-manifest.f913690eb44e54d2467aa50ba5774cb2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0c78d73c56151374d8d7a0b4a54f67",
+    "url": "/afang223/helloWorld.git/index.html"
+  },
+  {
+    "revision": "3633ebe237281119e06a",
+    "url": "/afang223/helloWorld.git/static/css/2.bbdea5ac.chunk.css"
+  },
+  {
+    "revision": "c8f640330953eb53793b",
+    "url": "/afang223/helloWorld.git/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "3633ebe237281119e06a",
+    "url": "/afang223/helloWorld.git/static/js/2.70fe6c11.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/afang223/helloWorld.git/static/js/2.70fe6c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f640330953eb53793b",
+    "url": "/afang223/helloWorld.git/static/js/main.a7c25418.chunk.js"
+  },
+  {
+    "revision": "15c848b7584923682ec7",
+    "url": "/afang223/helloWorld.git/static/js/runtime-main.9b8895c6.js"
+  },
+  {
+    "revision": "b567951bfef97beb676e6cd5968c4c8f",
+    "url": "/afang223/helloWorld.git/static/media/AlkesSemiBd.b567951b.woff"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/afang223/helloWorld.git/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/afang223/helloWorld.git/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/afang223/helloWorld.git/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/afang223/helloWorld.git/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "e7ccb0aa43af9fb7fae4d1bdc23a099b",
+    "url": "/afang223/helloWorld.git/static/media/NotoSans.e7ccb0aa.woff"
+  },
+  {
+    "revision": "5b7a2b952b64e8e829ef5c906f233e25",
+    "url": "/afang223/helloWorld.git/static/media/Resume.5b7a2b95.pdf"
+  },
+  {
+    "revision": "56f885380a0ae1fccc171d4419bfc7cd",
+    "url": "/afang223/helloWorld.git/static/media/abcentDemo.56f88538.mp4"
+  },
+  {
+    "revision": "b1ffd4369602f696dd4c2701524599c2",
+    "url": "/afang223/helloWorld.git/static/media/background.b1ffd436.jpg"
+  },
+  {
+    "revision": "989e7d71d66f6677cdc973c939ca7bc3",
+    "url": "/afang223/helloWorld.git/static/media/blueMountains.989e7d71.jpg"
+  },
+  {
+    "revision": "646403081e5e3c0a8b781a7b9132f6fd",
+    "url": "/afang223/helloWorld.git/static/media/email.64640308.png"
+  },
+  {
+    "revision": "a43cabf7a25b91dc1e62445277c3f76c",
+    "url": "/afang223/helloWorld.git/static/media/githubLogo.a43cabf7.png"
+  },
+  {
+    "revision": "acb5d0d394e8c0700e0adac722122036",
+    "url": "/afang223/helloWorld.git/static/media/gitletDemo.acb5d0d3.mp4"
+  },
+  {
+    "revision": "6e909369438f75cc78a2b301b95da083",
+    "url": "/afang223/helloWorld.git/static/media/linkedin.6e909369.png"
+  },
+  {
+    "revision": "1a89ac2db70d8e15a9c97aff5b83b094",
+    "url": "/afang223/helloWorld.git/static/media/munich.1a89ac2d.woff"
+  },
+  {
+    "revision": "ff3bca6fc20fb78e59b9b4b54a0e24c7",
+    "url": "/afang223/helloWorld.git/static/media/profile.ff3bca6f.png"
+  },
+  {
+    "revision": "054d98516dd6728454c2378e3cacce74",
+    "url": "/afang223/helloWorld.git/static/media/schemeDemo.054d9851.mp4"
+  },
+  {
+    "revision": "f20721456ea73dafc7beb2b4ca5ef873",
+    "url": "/afang223/helloWorld.git/static/media/separator.f2072145.jpg"
+  },
+  {
+    "revision": "f4d82d61dc3e950f7383d5d2445f8c6b",
+    "url": "/afang223/helloWorld.git/static/media/sportableDemo.f4d82d61.mp4"
+  },
+  {
+    "revision": "6d4913bcf04c5791492d2799fc9ada03",
+    "url": "/afang223/helloWorld.git/static/media/stockDemo.6d4913bc.mp4"
+  },
+  {
+    "revision": "2f2f5917d123c2f7c0466c1420dd4ae7",
+    "url": "/afang223/helloWorld.git/static/media/tranquillDemo.2f2f5917.mp4"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/afang223/helloWorld.git/precache-manifest.f913690eb44e54d2467aa50ba5774cb2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/afang223/helloWorld.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
